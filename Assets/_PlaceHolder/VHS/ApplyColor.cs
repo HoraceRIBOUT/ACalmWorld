a222f94b4e8108c0c405afa64ba7bc7474f15735
RIBOUT <pifi77@hotmail.fr>
2019-03-16 20:08:59 +0100

    [ADD] Apply mat to screen (first test)
    Basic code + tint color (who can be change by ApplyColor script)

---
+++ Assets/_PlaceHolder/VHS/ApplyColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyColor : ApplyMatToScreen
+{
+    public static ApplyColor instance;
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+           //  Destroy(this);  //because lol it's just a debug !
+        }
+    }
+
+
+    public void ChangeColorOfScreen(Color value)
+    {
+        matToApply.SetColor("_Color", value);
+    }
+}
---
+++ Assets/_PlaceHolder/VHS/ApplyMatToScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ApplyMatToScreen : MonoBehaviour
+{
+    public Material matToApply;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+#if !UNITY_EDITOR
+        if(Time.fixedTime > 3f) {
+#endif
+        if (matToApply != null)
+        {
+            RenderTexture tmp = destination;
+            Graphics.Blit(source, tmp, matToApply);
+            Graphics.Blit(tmp, destination);
+        }
+        else
+            Graphics.Blit(source, destination);
+#if !UNITY_EDITOR
+        }
+        else
+            Graphics.Blit(source, destination);
+#endif
+    }
+}
