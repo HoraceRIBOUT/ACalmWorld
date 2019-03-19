6c04c523c917fd12a394ea3be456b99fecf61f0c
RIBOUT <pifi77@hotmail.fr>
2019-03-19 21:13:14 +0100

    [GIT-DEBUG] Part 1

--- Assets/Scripts/Cube5.cs
+++ Assets/Scripts/Cube5.cs
@@ -7,6 +7,21 @@ public class Cube5 : TouchOnOff
 
     public override void Touched(bool on)
     {
-        //put the code for Wwise here
+        if (on)
+        {
+            AkSoundEngine.PostEvent("UnMute_ACW_Lofi_Musique1_Synth_Low", gameObject);
+        }
+
+        else
+        {
+            AkSoundEngine.PostEvent("Mute_ACW_Lofi_Musique_Synth_Low_01_Lp", gameObject);
+        }
+    }
+    public void Update()
+    {
+        int type = 1;
+        AkSoundEngine.GetRTPCValue(2478286086U, gameObject, 0, out ColorIntensity, ref type);
+
+        base.ChangeColor();
     }
 }
