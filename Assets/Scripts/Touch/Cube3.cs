fe137b9f6da71c17655cd020d866630ee920d866
RIBOUT <pifi77@hotmail.fr>
2019-03-26 13:53:21 +0100

    [UPDATE] Switch and states

--- Assets/Scripts/Touch/Cube3.cs
+++ Assets/Scripts/Touch/Cube3.cs
@@ -4,24 +4,4 @@ using UnityEngine;
 
 public class Cube3 : TouchOnOff
 {
-
-    public override void Touched(bool on)
-    {
-        if (on)
-        {
-            AkSoundEngine.PostEvent("UnMute_ACW_Lofi_Musique1_Synth_Aerie", gameObject);
-        }
-
-        else
-        {
-            AkSoundEngine.PostEvent("Mute_ACW_Lofi_Musique_Synth_Aerie_01_Lp", gameObject);
-        }
-    }
-    public void Update()
-    {
-        int type = 1;
-        AkSoundEngine.GetRTPCValue(56683494U, gameObject, 0, out ColorIntensity, ref type);
-
-        base.ChangeColor();
-    }
 }
