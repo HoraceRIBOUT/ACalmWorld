7ffc67fd7843226f46f3edcef1b84cad83619066
RIBOUT <pifi77@hotmail.fr>
2019-03-27 10:47:11 +0100

    [ADD] Particle maker

---
+++ Assets/Scripts/Touch/Change/ParticleMaker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleMaker : Change
+{
+
+    public List<ParticleSystem> particleForEachState = new List<ParticleSystem>();
+    private int previousState = -1;
+
+    public override void ChangeOnStart()
+    {
+        previousState = -1;
+    }
+
+    public override void ChangeOnClick(int currentState, bool on)
+    {
+        if(previousState != -1 )
+            particleForEachState[previousState].Stop();
+        if(on)
+            particleForEachState[currentState].Play();
+        previousState = currentState;
+    }
+
+    public override void ChangeOnUpdate(float rtpcValue)
+    {
+
+    }
+}
