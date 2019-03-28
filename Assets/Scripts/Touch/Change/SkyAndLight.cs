be8931a9ad5bbeac8fbcc5fbb19c057a516dbfd1
RIBOUT <pifi77@hotmail.fr>
2019-03-28 18:53:02 +0100

    [UPDATE] Sound work

---
+++ Assets/Scripts/Touch/Change/SkyAndLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyAndLight : Animated
+{
+    public Material skybox;
+    public Light ambientLight;
+
+    public Color skyboxGround = Color.white;
+    public Color skyboxSky = Color.white;
+    public Color ambient = Color.white;
+    
+    public override void ChangeOnUpdate(float rtpcValue)
+    {
+        skybox.SetColor("_SkyTint", skyboxSky);
+        skybox.SetColor("_GroundColor", skyboxGround);
+        RenderSettings.skybox = skybox;
+        ambientLight.color = ambient;
+        RenderSettings.ambientLight = ambient;
+    }
+
+}
