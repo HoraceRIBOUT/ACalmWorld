e1a164bf7042c4e44a120a3bd1164209998a22b8
RIBOUT <pifi77@hotmail.fr>
2019-03-30 20:41:32 +0100

    [UPDATE] Skies effect

--- Assets/Scripts/Touch/Change/SkyAndLight.cs
+++ Assets/Scripts/Touch/Change/SkyAndLight.cs
@@ -7,17 +7,32 @@ public class SkyAndLight : Animated
     public Material skybox;
     public Light ambientLight;
 
-    public Color skyboxGround = Color.white;
     public Color skyboxSky = Color.white;
-    public Color ambient = Color.white;
+    public Color skyboxEquator = Color.white;
+    public Color skyboxGround = Color.white;
+    public Color lightColor = Color.white;
+
     
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (!UnityEditor.EditorApplication.isPlaying)
+        {
+            ChangeOnUpdate(0.0f);
+            Debug.Log("Change validate");
+        }
+    }
+#endif 
+    
+
     public override void ChangeOnUpdate(float rtpcValue)
     {
         skybox.SetColor("_SkyTint", skyboxSky);
         skybox.SetColor("_GroundColor", skyboxGround);
         RenderSettings.skybox = skybox;
-        ambientLight.color = ambient;
-        RenderSettings.ambientLight = ambient;
+        ambientLight.color = lightColor;
+        RenderSettings.ambientSkyColor = skyboxSky;
+        RenderSettings.ambientEquatorColor = skyboxEquator;
+        RenderSettings.ambientGroundColor = skyboxGround;
     }
-
 }
