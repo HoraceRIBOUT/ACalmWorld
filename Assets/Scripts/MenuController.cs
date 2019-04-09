23c131b48c55bec1c89d423e29254581495da9c0
Damoone <ambroise.durand@gmail.com>
2019-04-09 17:36:04 +0200

    Add menu scene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Text timeText;
+    public Text dateText;
+    public GameObject panelOption;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dateText.text = DateTime.Now.ToString().Substring(0, 6)+"????";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeText.text = System.DateTime.Now.TimeOfDay.ToString().Substring(0, 5);
+    }
+
+    public void OnPushPlay()
+    {
+        SceneManager.LoadScene("FirstCompo");
+    }
+
+    public void OnPushQuit()
+    {
+        Application.Quit();
+    }
+
+    public void OnPushOption()
+    {
+        panelOption.SetActive(true);
+    }
+}
