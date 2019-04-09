937ee054c029df9c86c2dfe7bc814a4940d0c57e
RIBOUT <pifi77@hotmail.fr>
2019-04-09 16:32:14 +0200

    [UPDATE] Base for Simple VHS effect

---
+++ Assets/Scripts/PostProcess/ApplyVHS.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ApplyVHS : MonoBehaviour
+{
+    public Material matToApply;
+    
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (matToApply != null)
+        {
+            RenderTexture tmp = destination;
+            Graphics.Blit(source, tmp, matToApply);
+            Graphics.Blit(tmp, destination);
+        }
+    }
+}
