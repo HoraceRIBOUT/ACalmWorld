a6c9acb1c5799da81596f347931d513dd9fe7ba3
RIBOUT <pifi77@hotmail.fr>
2019-04-23 15:16:44 +0200

    [ADD] Music preparation for the menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -11,10 +11,17 @@ public class MenuController : MonoBehaviour
     public Text dateText;
     public GameObject panelOption;
 
+    public AK.Wwise.Event startEvent;
+    public AK.Wwise.Event clickEvent;
+    public AK.Wwise.Event launchGameEvent;
+
     // Start is called before the first frame update
     void Start()
     {
-        dateText.text = DateTime.Now.ToString().Substring(0, 6)+"????";
+        dateText.text = DateTime.Now.ToString().Substring(0, 6)+"19??";
+
+        if(startEvent.IsValid())
+            AkSoundEngine.PostEvent(startEvent.Id, this.gameObject);
     }
 
     // Update is called once per frame
@@ -25,6 +32,9 @@ public class MenuController : MonoBehaviour
 
     public void OnPushPlay()
     {
+        //need to have a little animation / glitch here before launching (load in a dark screen would be perfect)
+        if (launchGameEvent.IsValid())
+            AkSoundEngine.PostEvent(launchGameEvent.Id, this.gameObject);
         SceneManager.LoadScene("FirstCompo");
     }
 
@@ -36,5 +46,11 @@ public class MenuController : MonoBehaviour
     public void OnPushOption()
     {
         panelOption.SetActive(true);
+    }
+
+    public void PlayButtonSound()
+    {
+        if (clickEvent.IsValid())
+            AkSoundEngine.PostEvent(clickEvent.Id, this.gameObject);
     }
 }
