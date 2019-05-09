e4577b3a96257cb0669154053fe1533c1aa17176
RIBOUT <pifi77@hotmail.fr>
2019-05-09 11:22:40 +0200

    [DEBUG] retour neon sprite

---
+++ Assets/Scripts/Touch/Change/SpriteChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChange : Change
+{
+    public SpriteRenderer spriteRdr;
+
+    public List<Sprite> spritePerState;
+
+    public override void AddEventOnListener(MainInstrument mI)
+    {
+        mI.onClickEvent.AddListener(ChangeOnClick);
+        mainInstrument = mI;
+    }
+
+    public void ChangeOnClick()
+    {
+        int currState = mainInstrument.instruData.currentState;
+        if (spriteRdr != null)
+        {
+            spriteRdr.sprite = spritePerState[currState];
+        }
+    }
+}
