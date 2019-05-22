5daffc34b3608aea8d6f3dad9130a2c27510f817
Damoone <ambroise.durand@gmail.com>
2019-05-22 14:14:36 +0200

    Correction of the yes/no button + deleted the menu vhs shader

---
+++ Assets/Scripts/OptionController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OptionController : MonoBehaviour
+{
+    public GameObject YesLockButton;
+    public GameObject NoLockButton;
+    public AK.Wwise.Event clickEvent;
+
+
+    public void pressYesButton()
+    {
+        if (PlayerPrefs.GetInt("AlwaysPlaying",0) == 0 )
+        {
+            PlayerPrefs.SetInt("AlwaysPlaying", 1 );
+            YesLockButton.GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Underline;
+            NoLockButton.GetComponent<TextMeshProUGUI>().fontStyle &= ~FontStyles.Underline;
+            AkSoundEngine.PostEvent(clickEvent.Id, this.gameObject);
+        }
+    }
+
+    public void pressNoButton()
+    {
+        if (PlayerPrefs.GetInt("AlwaysPlaying", 0) == 1)
+        {
+            PlayerPrefs.SetInt("AlwaysPlaying", 0);
+            NoLockButton.GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Underline;
+            YesLockButton.GetComponent<TextMeshProUGUI>().fontStyle &= ~FontStyles.Underline;
+            AkSoundEngine.PostEvent(clickEvent.Id, this.gameObject);
+        }
+    }
+}
